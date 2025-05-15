b4c577e90ddbfbeec301ea76bf92f4695a429f0f
VendettaVVV <s10545kb@ms.wwsi.edu.pl>
2025-05-15 20:47:18 +0200

    Added conection to db in Invoice service

---
+++ invoiceService/Models/productInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InvoiceService.Models
+{
+    [Table("produktinfo")]
+    public class ProductInfo
+    {
+        [Key]
+        [Column("id")]
+        public int id { get; set; }
+
+        [Column("ilosc")]
+        public string quantity { get; set; }
+
+        [Column("cenaogolem")]
+        public double totalPrice { get; set; }
+
+        [Column("podatek")]
+        public int tax { get; set; }
+
+        [Column("kwotapodatku")]
+        public double taxAmount { get; set; }
+
+        [Column("netto")]
+        public double net { get; set; }
+
+        [Column("brutto")]
+        public double gross { get; set; }
+
+        [Column("faktura_id")]
+        public int invoiceId { get; set; }
+
+        [ForeignKey("InvoiceId")]
+        public Invoice invoice { get; set; }
+    }
+}
