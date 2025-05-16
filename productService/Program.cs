5fdf77df2b7e7c2026e79b30721dc74b525c7387
kapi2334 <kapi2334@gmail.com>
2025-05-17 00:37:26 +0200

    Repaired productService dbContext error.

--- productService/Program.cs
+++ productService/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate(); // <- to wykona migracje
 }
 
