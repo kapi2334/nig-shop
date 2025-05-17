0263f79e91dc9ee26d897db736b10e7c19ed79bd
VendettaVVV <s10545kb@ms.wwsi.edu.pl>
2025-05-17 20:46:55 +0200

    Modifications in UI of ClientApp

--- Frontend/clientApp/Components/Models.cs
+++ Frontend/clientApp/Components/Models.cs
@@ -1,8 +1,39 @@
+namespace NigShop.Shared.Models;
+
 public class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; } = "";
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public double Weight { get; set; }
     public decimal Price { get; set; }
+    public decimal Tax { get; set; }
+    public int DimensionsId { get; set; }
+    public Dimensions Dimensions { get; set; } = new();
+    public int MaterialId { get; set; }
+    public Material Material { get; set; } = new();
+    public int SurfaceTypeId { get; set; }
+    public SurfaceType SurfaceType { get; set; } = new();
+}
+
+public class Dimensions
+{
+    public int Id { get; set; }
+    public double Length { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
+
+public class Material
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
+
+public class SurfaceType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
 
 public class CartItem
