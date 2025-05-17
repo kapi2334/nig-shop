fe147db3f45683ab97b56acf493b079d46b0eef0
kapi2334 <kapi2334@gmail.com>
2025-05-17 15:19:06 +0200

    Issurer api in InvoiceAPI done

--- invoiceService/Endpoints/IssuersEndpoints.cs
+++ invoiceService/Endpoints/IssuersEndpoints.cs
@@ -28,6 +28,35 @@ namespace InvoiceService.Endpoints{
             .WithName("GetIssuer")
             .WithOpenApi();
 
+            endpoints.MapPost("/issuer", async (Issuer input, AppDbContext db) =>
+            {
+                if (!db.Database.CanConnect())
+                {
+                    return Results.Problem(
+                        detail: $"Can't connect to invoiceService database. Connection: {db.Database.CanConnect()}",
+                        statusCode: StatusCodes.Status503ServiceUnavailable
+                    );
+                }
+                
+                    var items = db.issuer
+                    .OrderByDescending(x => x.id)
+                    .FirstOrDefault();
+                //Only add issuer if not exist one.
+                if (items is not null)
+                {
+                    return Results.Problem(
+                        detail: "Before adding new issuer, delete existing one.",
+                        statusCode: StatusCodes.Status409Conflict
+                            );
+                }
+                else
+                {
+                    
+                    return Results.NotFound();
+                }
+            })
+            .WithName("AddIssuer")
+            .WithOpenApi();
             return endpoints;
         }
 
