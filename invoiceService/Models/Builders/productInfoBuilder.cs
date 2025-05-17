f1f75f8730e7e9f9562d62886ef6ab3bc0091545
kapi2334 <kapi2334@gmail.com>
2025-05-17 19:58:49 +0200

    ProductInfoBuilder created in invoiceService

---
+++ invoiceService/Models/Builders/productInfoBuilder.cs
@@ -0,0 +1,67 @@
+namespace InvoiceService.Models.Builders{
+
+    public class ProductInfoBuilder{
+
+        private readonly ProductInfo _productInfo;
+
+        public ProductInfoBuilder()
+        {
+            _productInfo = new ProductInfo();
+        }
+
+
+        public ProductInfoBuilder WithQuantity(string quantity)
+        {
+            _productInfo.quantity = quantity;
+            return this;
+        }
+
+        public ProductInfoBuilder WithTotalPrice(double totalPrice)
+        {
+            _productInfo.totalPrice = totalPrice;
+            return this;
+        }
+
+        public ProductInfoBuilder WithTax(int tax)
+        {
+            _productInfo.tax = tax;
+            return this;
+        }
+
+        public ProductInfoBuilder WithTaxAmount(double taxAmount)
+        {
+            _productInfo.taxAmount = taxAmount;
+            return this;
+        }
+
+        public ProductInfoBuilder WithNet(double net)
+        {
+            _productInfo.net = net;
+            return this;
+        }
+
+        public ProductInfoBuilder WithGross(double gross)
+        {
+            _productInfo.gross = gross;
+            return this;
+        }
+
+        public ProductInfoBuilder WithInvoiceId(int invoiceId)
+        {
+            _productInfo.invoiceId = invoiceId;
+            return this;
+        }
+
+        public ProductInfoBuilder WithProductId(int productId)
+        {
+            _productInfo.product_id = productId;
+            return this;
+        }
+
+        public ProductInfo Build()
+        {
+            return _productInfo;
+        }
+    }
+}
+
